67ffba4f264a1ea32671fd821daa1f50a31af9c0
amirhallaji <a.hallaji.b@gmail.com>
2020-05-02 13:04:54 +0430

    progressDialog() completed. secondaryProgreeDialog Added.

--- app/src/main/java/com/example/dialogproject/MainActivity.java
+++ app/src/main/java/com/example/dialogproject/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setMessage("please wait...");
         progressDialog.setCancelable(false);
         final long progress = 3000L ;
+        long secondaryProgress = 1500L;
 //        new Handler().postDelayed(new Runnable() {
 //            @Override
 //            public void run() {
@@ -35,38 +36,58 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        },progress);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-//        new Timer().scheduleAtFixedRate(new TimerTask() {
-//            @Override
-//            public void run() {
-//                if(progressDialog.getProgress() < progressDialog.getMax()){
-//                    progressDialog.incrementProgressBy(2);
-//                }
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(progressDialog.getProgress() < progressDialog.getMax()) {
+                    progressDialog.incrementProgressBy(12);
+                }
 //                else {
 //                    progressDialog.dismiss();
 //                }
-//            }
-//        },0,progress);
+            }
+        }, 0, progress);
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(progressDialog.getProgress() < progressDialog.getMax()){
+                    progressDialog.incrementSecondaryProgressBy(20);
+                }
+            }
+        },0,secondaryProgress);
         progressDialog.show();
     }
+
+
+
     public void showAlertDialog(View view){
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        //type 1
-        alertDialog.setTitle("Alert Dialog")
-                .setMessage("Do you want to delete this file ?")
-                .setCancelable(false)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this,"files deleted",Toast.LENGTH_LONG).show();
-                    }
-                })
-                .setNegativeButton("NO" , null)
-                .setNeutralButton("Cancel",null)
-                .show();
+//        //type 1
+//        alertDialog.setTitle("Alert Dialog")
+//                .setMessage("Do you want to delete this file ?")
+//                .setCancelable(false)
+//                .setIcon(android.R.drawable.ic_dialog_alert)
+//                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(MainActivity.this,"files deleted",Toast.LENGTH_LONG).show();
+//                    }
+//                })
+//                .setNegativeButton("NO" , null)
+//                .setNeutralButton("Cancel",null)
+//                .show();
 
         //type 2
-
+        alertDialog.setCancelable(false)
+                .setTitle("Question")
+                .setSingleChoiceItems(new String[]{"A", "B", "C"}, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"which : " + which,Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("Ok",null)
+                .show();
     }
 
 
